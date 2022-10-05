b7a3dc4616925aa898af0c285e22f65dec1cc32e
Jimbei <contrerasr2272@gmail.com>
2022-10-05 16:24:53 -0400

    setting up frontend react to connect to backend server

--- server/server.js
+++ server/server.js
@@ -1,4 +1,6 @@
 const express = require('express');
+//* we need to update the back-end server's code to serve up the React front-end code in production
+const path = require('path');
 // import ApolloServer
 const { ApolloServer } = require('apollo-server-express');
 
@@ -32,6 +34,15 @@ const startApolloServer = async (typeDefs, resolvers) => {
 // integrate our Apollo server with the Express application as middleware
 server.applyMiddleware({app});
 
+
+// Serve up static assets
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
 
 db.once('open', () => {
   app.listen(PORT, () => {
